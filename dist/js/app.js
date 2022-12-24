bbcfefd58c5ee1601cf13dfa41887e7a4caf3d9c
Sochuks <moseschukwumakajang@gmail.com>
2022-12-24 18:10:38 +0100

    - Get principal - Calculate interest - Calculate payment - Calculate monthly payment - calculate total interest - calculate total payment - Display result to boxes

--- dist/js/app.js
+++ dist/js/app.js
@@ -10,5 +10,27 @@ function computeResults(e){
     const UIrate = document.getElementById("rate").value;
     const UIyear = document.getElementById("year").value
 
-    console.log(UIamount, UIrate, UIyear)  
+    //Calculate
+    const principal = parseFloat(UIamount);
+    const calculateInterest = parseFloat(UIrate) / 100 / 12;
+    const calculatePayment = parseFloat(UIyear) * 12;
+
+    //compute monthly payment 
+    const x = Math.pow(1 + calculateInterest, calculatePayment);
+    const monthly = (principal * x * calculateInterest) / (x - 1);
+    const monthlyPayment = monthly.toFixed(2);
+
+    //compute interest
+     const totalInterest = (monthly * calculatePayment - principal).toFixed(2);
+
+     //compute total payment
+     const totalPayment = (monthly * calculatePayment).toFixed(2);
+
+     //show result 
+     document.getElementById("mp").innerHTML = "N" + monthlyPayment;
+
+     document.getElementById("ti").innerHTML = "%" + totalInterest;
+
+     document.getElementById("tp").innerHTML = "N" + totalPayment;
+
 }
